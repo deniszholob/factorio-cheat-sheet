182194d1caa2dde32282f2fe6a7965b917f68cff
Denis Zholob <dddgamer@live.com>
2017-09-16 23:54:28 -0400

    Add Overview section, add titles to sections

---
+++ src/app/views/overview/overview.component.ts
@@ -0,0 +1,21 @@
+// Angular Imports
+import { Component, OnInit } from '@angular/core';
+
+// Constants
+import { APP_SETTINGS } from 'app/shared/app-settings';
+
+@Component({
+    selector: 'app-overview',
+    templateUrl: './overview.component.html',
+    // styleUrls: ['./overview.component.scss'] // Enable as needed
+})
+export class OverviewComponent implements OnInit {
+
+    public APP_SETTINGS = APP_SETTINGS;
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
