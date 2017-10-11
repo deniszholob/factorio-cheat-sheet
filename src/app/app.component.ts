0542d83132db924665f1f436d72e4bccc0b13540
Denis <dddgamer@live.com>
2017-10-11 19:46:58 -0400

    Fix issue of not scrolling to a section specified by # in the url on load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,37 @@
 // Angular Imports
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+// Ongoing angular issue with scrolling to anchor elements: https://github.com/angular/angular/issues/6595
+// Modified Solution fromt the issues page and https://stackoverflow.com/questions/36101756/angular2-routing-with-hashtag-to-page-anchor
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     // styleUrls: ['./app.component.scss'] // Enable as needed
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, AfterViewChecked {
+    private fragment: string;
+    private scrolled = false;
+
+    constructor(private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        this.route.fragment.subscribe(fragment => { this.fragment = fragment; });
+    }
+
+    /**
+     * This will execute each time after every view is loaded
+     * Only need one correct view to load then stop trying to scroll
+     * Catch any invalid views.
+     */
+    ngAfterViewChecked(): void {
+        if (!this.scrolled) {
+            try {
+                document.querySelector('#' + this.fragment).scrollIntoView();
+                this.scrolled = true;
+            } catch (e) { } // Ignore any invalid tries
+        }
+    }
+
+}
