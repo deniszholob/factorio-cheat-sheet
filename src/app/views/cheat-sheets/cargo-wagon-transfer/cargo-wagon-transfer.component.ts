3540abc9a429c921448ddd97bb3b9fd07998b9bb
Denis <dddgamer@live.com>
2017-10-12 21:23:55 -0400

    Add Cargo Wagon Transfer Sheet

---
+++ src/app/views/cheat-sheets/cargo-wagon-transfer/cargo-wagon-transfer.component.ts
@@ -0,0 +1,42 @@
+// Angular Imports
+import { Component, OnInit } from '@angular/core';
+
+// Services
+import { DataService } from 'app/services/data.service';
+
+// Models
+import { Data } from 'app/services/data';
+import { CheatSheet } from 'app/shared/cheat-sheet/cheat-sheet.model';
+
+// Constants
+import { APP_SETTINGS } from 'app/shared/app-settings';
+const dataFile = 'cargo-wagon-transfer';
+
+@Component({
+    selector: 'app-cargo-wagon-transfer',
+    templateUrl: './cargo-wagon-transfer.component.html',
+    // styleUrls: ['./cargo-wagon-transfer.component.scss'] // Enable as needed
+})
+export class CargoWagonTransferComponent implements OnInit {
+    cheatSheet: CheatSheet;
+    sheetData: any;
+
+    APP_SETTINGS = APP_SETTINGS;
+
+    constructor(
+        public dataService: DataService
+    ) { }
+
+    ngOnInit() {
+        this.dataService.getCheatSheetData(dataFile).subscribe(
+            (result: Data) => {
+                this.cheatSheet = result.cheatSheet;
+                this.sheetData = result.data;
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
+
+}
