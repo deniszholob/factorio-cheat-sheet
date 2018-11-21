bdec9d60f5aa31b56b70accec45af03a6bdeca46
Daniel Alan Miller <dalan.miller@gmail.com>
2018-11-21 16:10:52 +1000

    Adding sw.js for offline use

---
+++ src/sw.js
@@ -0,0 +1,29 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    // Cache CSS files
+    /.*\.css/,
+    // Use cache but update in the background ASAP
+    workbox.strategies.staleWhileRevalidate({
+        // Use a custom cache name
+        cacheName: 'css-cache',
+    })
+);
+
+workbox.routing.registerRoute(
+    // Cache image files
+    /.*\.(?:png|jpg|jpeg|svg|gif)/,
+    // Use the cache if it's available
+    workbox.strategies.staleWhileRevalidate({
+        // Use a custom cache name
+        cacheName: 'image-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                // Cache only 20 images
+                maxEntries: 20,
+                // Cache for a maximum of a week
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+            })
+        ],
+    })
+);
