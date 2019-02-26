aaa1d2d8305899031c41a16d89c468d5139e41ae
Denis Zholob <dddgamer3x@gmail.com>
2019-02-26 14:21:00 -0700

    #102 PWA Service Worker
    * Add Angular Service worker to the app
    * Add page reaload onto the main component if there is an update from service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@
 import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+// PWA
+import { SwUpdate } from '@angular/service-worker';
+
 // Ongoing Angular issue with scrolling to anchor elements: https://github.com/angular/angular/issues/6595
 // Modified Solution from the issues page and https://stackoverflow.com/questions/36101756/angular2-routing-with-hashtag-to-page-anchor
 
@@ -14,7 +17,15 @@ export class AppComponent implements OnInit, AfterViewChecked {
     private fragment: string;
     private scrolled = false;
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(
+        private route: ActivatedRoute,
+        private updates: SwUpdate,
+    ) {
+        // Service worker auto refresh the page if new version available
+        updates.available.subscribe(event => {
+            this.updates.activateUpdate().then(() => document.location.reload());
+        });
+    }
 
     ngOnInit() {
         this.route.fragment.subscribe(fragment => { this.fragment = fragment; });
