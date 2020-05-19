ff4356d7bf45d2e44966ee2a8acb899465a2da48
Denis Zholob <dddgamer3x@gmail.com>
2020-05-19 18:27:19 -0400

    Update Angular to v9 (#213)
    * Run angular upgrades to v9
    * Update node version
    * Optimize some images
    * Fix and format styles

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -77,3 +77,8 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+/******************************************************************
+ * Load `$localize` - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
