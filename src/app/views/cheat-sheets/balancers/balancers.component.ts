6ea997b3a926ea58c146601c69b1a3c7947dc63b
Denis Zholob <dddgamer3x@gmail.com>
2020-06-13 22:31:13 -0400

    Closes #232 - Update Balancer Page (#237)
    * Replace balancer table with tips
    * Add 4x4 and 8x8 balancer links
    * Reorder Balancer footer link section

--- src/app/views/cheat-sheets/balancers/balancers.component.ts
+++ src/app/views/cheat-sheets/balancers/balancers.component.ts
@@ -5,6 +5,9 @@ import { HttpClient } from '@angular/common/http';
 // RXJS
 import { forkJoin } from 'rxjs';
 
+// UI
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
+
 // Services
 import { DataService, ClipboardService, BASE_URL } from 'app/services/index';
 
@@ -28,10 +31,12 @@ export class BalancersComponent implements OnInit {
     public cheatSheet: CheatSheet;
     public sheetData: any;
 
-    /** Tooltips for copying blueprint book */
-    public tooltipMsgs: string[] = [];
+    /** Tooltip for copying blueprint book */
+    public tooltipMsg = tooltip_Default;
     /** Holds the blueprint strings for user to copy */
     public blueprintStrings: string[] = [];
+    /** Holds the blueprint strings for user to copy */
+    public blueprintTable: string[] = [];
 
     public APP_SETTINGS = APP_SETTINGS;
 
@@ -59,14 +64,31 @@ export class BalancersComponent implements OnInit {
      * Populate the tooltip array with default string
      */
     private async processBlueprints() {
-        const streams = [];
-        this.sheetData.table.forEach(blueprintBook => {
-            this.tooltipMsgs.push(tooltip_Default);
-            streams.push(this.httpClientService.get(BASE_URL + blueprintBook.url, { responseType: 'text' }));
+        this.tooltipMsg = tooltip_Default;
+        const blueprintStreams = [];
+        const tableStreams = [];
+
+        this.sheetData.commonBalancers.forEach(blueprint => {
+            blueprintStreams.push(this.httpClientService.get(blueprint.raw, { responseType: 'text' }));
         });
-        forkJoin(streams).subscribe(
+        this.sheetData.table.forEach(blueprintBook => {
+            tableStreams.push(this.httpClientService.get(BASE_URL + blueprintBook.url, { responseType: 'text' }));
+        });
+
+        // Common links
+        forkJoin(blueprintStreams).subscribe(
             (response: string[]) => {
                 this.blueprintStrings = response;
+            },
+            error => {
+                console.log(error);
+            }
+        );
+
+        // Old Table
+        forkJoin(tableStreams).subscribe(
+            (response: string[]) => {
+                this.blueprintTable = response;
             },
             error => {
                 console.log(error);
@@ -76,23 +98,26 @@ export class BalancersComponent implements OnInit {
     }
 
     /**
-     * Resets the tooltip message to the default after a timeout
-     * @param i Index of the message to reset
+     * Resets the tooltip message to the default and close after a timeout
+     * @param tooltip NgbTooltip object to operate on
      */
-    private resetTooltip(i: number): void {
+    private resetTooltip(tooltip: NgbTooltip): void {
         setTimeout(() => {
-            this.tooltipMsgs[i] = tooltip_Default;
+            this.tooltipMsg = tooltip_Default;
+            tooltip.close();
+            console.log(this.tooltipMsg);
         }, 2000);
     }
 
     /**
      * Copies Blueprint to clipboard
      * @param url Link that contains the BP text
+     * @param tooltip NgbTooltip object to operate on
      */
-    public copyBpString(url: string, i: number): void {
-        this.clipboardService.copyMessageExec(this.blueprintStrings[i]);
-        this.tooltipMsgs[i] = tooltip_Copied;
-        this.resetTooltip(i);
+    public copyBpString(url: string, tooltip: NgbTooltip): void {
+        this.clipboardService.copyMessageExec(url);
+        this.tooltipMsg = tooltip_Copied;
+        this.resetTooltip(tooltip);
     }
 
     /**
