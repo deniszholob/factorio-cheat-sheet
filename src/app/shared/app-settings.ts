efcf39ea5d5150884f323ee8bdab1a69696b6aca
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-07-06 16:14:43 +0200

    Bump Factorio version from 0.18.34 to 0.18.35

--- src/app/shared/app-settings.ts
+++ src/app/shared/app-settings.ts
@@ -15,7 +15,7 @@ export const APP_SETTINGS: AppInfo = {
         name: 'Github Source',
         url: '//github.com/deniszholob/factorio-cheat-sheet',
     },
-    data_version: '0.17.79, 0.18.34',
+    data_version: '0.17.79, 0.18.35',
     // languages: ['en-US'],
     links: {
         getLocalImagePath: function (imageName) {
