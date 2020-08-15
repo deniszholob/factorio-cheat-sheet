8ba94d84e80b6d237770f1b1707b4311f91d1b0f
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-08-15 12:48:03 +0200

    Change Factorio version 1.0 -> 1.0.0

--- src/app/shared/app-settings.ts
+++ src/app/shared/app-settings.ts
@@ -15,7 +15,7 @@ export const APP_SETTINGS: AppInfo = {
         name: 'Github Source',
         url: 'https://github.com/deniszholob/factorio-cheat-sheet',
     },
-    data_version: '1.0',
+    data_version: '1.0.0',
     // languages: ['en-US'],
     links: {
         getLocalImagePath: function (imageName) {
