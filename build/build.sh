a4af958ca117c16c55047c763b3080ab6b95e666
Denis Zholob <dddgamer3x@gmail.com>
2022-12-16 11:39:33 -0700

    Add service worker back in and fix build

--- build/build.sh
+++ build/build.sh
@@ -9,7 +9,7 @@
 echo ' -- Build Script Started ...'
 sh ./build/update-date.sh
 echo ' -- Running npm build script ...'
-npm run build
+npm run build:prod
 echo ' -- Build contents ...'
 ls -al "public"
 echo ' -- Build script finished ...'
