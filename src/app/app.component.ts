a302d8bb6ff9fdb6ed080d4e620bb0ed94e92dbb
Denis Zholob <dddgamer3x@gmail.com>
2022-12-26 18:20:07 -0700

    #361 Tyry to increase performance
    * Add on destroy unsub
    * Add service worker wiki image urls
    * Enable native angular anchor scrolling
    * Disable ngbcollapse animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 // Angular
-import { Component, OnInit, AfterViewChecked } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
 
 // PWA
 import {
@@ -8,7 +7,7 @@ import {
   VersionEvent,
   VersionReadyEvent,
 } from '@angular/service-worker';
-import { filter, map } from 'rxjs';
+import { filter, map, Subject, takeUntil } from 'rxjs';
 
 // Ongoing Angular issue with scrolling to anchor elements: https://github.com/angular/angular/issues/6595
 // Modified Solution (ngOnInit() and ngAfterVewInit()) from the issues page and https://stackoverflow.com/questions/36101756/angular2-routing-with-hashtag-to-page-anchor
@@ -18,34 +17,46 @@ import { filter, map } from 'rxjs';
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.scss'] // Enable as needed
 })
-export class AppComponent implements OnInit, AfterViewChecked {
-  private fragment: string = '';
-  private scrolled: boolean = false;
+export class AppComponent implements OnDestroy {
+  // private fragment: string = '';
+  // private scrolled: boolean = false;
 
-  constructor(private route: ActivatedRoute, private swUpdate: SwUpdate) {
+  private clearSub$ = new Subject<void>();
+
+  constructor(
+    // private route: ActivatedRoute,
+    private swUpdate: SwUpdate
+  ) {
     this.subToUpdateApp();
   }
 
-  ngOnInit(): void {
-    this.route.fragment.subscribe((fragment: string | null) => {
-      this.fragment = fragment ?? '';
-    });
-  }
+  // ngOnInit(): void {
+  // this.route.fragment
+  //   .pipe(takeUntil(this.clearSub$))
+  //   .subscribe((fragment: string | null) => {
+  //     this.fragment = fragment ?? '';
+  //   });
+  // }
 
   /**
    * This will execute each time after every view is loaded
    * Only need one correct view to load then stop trying to scroll
    * Catch any invalid views.
    */
-  ngAfterViewChecked(): void {
-    if (!this.scrolled) {
-      try {
-        document.querySelector('#' + this.fragment)?.scrollIntoView();
-        this.scrolled = true;
-      } catch (e) {
-        // Ignore any invalid tries
-      }
-    }
+  // ngAfterViewChecked(): void {
+  // if (!this.scrolled) {
+  //   try {
+  //     document.querySelector('#' + this.fragment)?.scrollIntoView();
+  //     this.scrolled = true;
+  //   } catch (e) {
+  //     // Ignore any invalid tries
+  //   }
+  // }
+  // }
+
+  ngOnDestroy(): void {
+    this.clearSub$.next();
+    this.clearSub$.complete();
   }
 
   /** Service worker auto refresh the page if new version available */
@@ -58,6 +69,7 @@ export class AppComponent implements OnInit, AfterViewChecked {
   /** https://angular.io/api/service-worker/SwUpdate */
   private checkServiceWorkerForUpdates() {
     return this.swUpdate.versionUpdates.pipe(
+      takeUntil(this.clearSub$),
       filter(
         (evt: VersionEvent): evt is VersionReadyEvent =>
           evt.type === 'VERSION_READY'
