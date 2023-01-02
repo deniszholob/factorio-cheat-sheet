f689269859e383d6d55a0fdd8b3ba2e632b75274
Denis Zholob <dddgamer3x@gmail.com>
2023-01-01 20:52:33 -0700

    Support deep linking to github pages like  /something
    * Per angular docs, copy index to 404 page using the build script
    * Remove linting from build, add as a separate command
    * Add text wrapping to select field

--- build/build.sh
+++ build/build.sh
@@ -8,8 +8,15 @@
 
 echo ' -- Build Script Started ...'
 sh ./build/update-date.sh
+
 echo ' -- Running npm build script ...'
 npm run build:prod
+
+# https://angular.io/guide/deployment#deploy-to-github-pages
+echo ' -- Copy index.html to 404.html ...'
+cp public/index.html public/404.html
+
 echo ' -- Build contents ...'
 ls -al "public"
+
 echo ' -- Build script finished ...'
