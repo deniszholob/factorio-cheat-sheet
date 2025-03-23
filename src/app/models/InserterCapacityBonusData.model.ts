327c7498c774da335b6ccf25e7b39bd077b5344c
Denis Zholob <dddgamer3x@gmail.com>
2025-03-23 16:28:43 -0600

    #424 Update inserter capacity bonus
    * Add to pre-commit code to find nvm
    * Remove old filter insterers
    * Add new bulk inserter entry and data for stack inserters

--- src/app/models/InserterCapacityBonusData.model.ts
+++ src/app/models/InserterCapacityBonusData.model.ts
@@ -6,6 +6,8 @@ export interface TechBonus {
   tech: string;
   nonStackItems: string;
   nonStackBonus: string;
+  bulkItems: string;
+  bulkBonus: string;
   stackItems: string;
   stackBonus: string;
 }
